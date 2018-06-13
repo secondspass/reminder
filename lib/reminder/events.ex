7667d07a70dc4a33763cdff259ccb037ec684485
Subil <subil1407@gmail.com>
2018-06-12 21:33:22 -0400

    wrote some tests. Are we finally doing tdd now?

---
+++ lib/reminder/events.ex
@@ -0,0 +1,1 @@
+# case filter_events do no_events -> quit (or) [listofevents] -> create_msg |> send_email
--- test/reminder_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ReminderTest do
-  use ExUnit.Case
-  doctest Reminder
-
-  test "greets the world" do
-    assert Reminder.hello() == :world
-  end
-end
