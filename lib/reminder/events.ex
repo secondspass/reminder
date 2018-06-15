e21395ddf2105498a664f25e61f10af217bf85d8
Subil <subil1407@gmail.com>
2018-06-15 13:41:41 -0400

    Completed create_message and filter_events and tests

--- lib/reminder/events.ex
+++ lib/reminder/events.ex
@@ -1,1 +1,51 @@
-# case filter_events do no_events -> quit (or) [listofevents] -> create_msg |> send_email
+# also see if date sigils can be read and written in ets and dets tables
+defmodule Reminder.Events do
+
+  # def send_events_reminder(event_data) do
+  #   case filter_events(event_data) do
+  #     :no_events -> quit
+  #     list_of_events -> list_of_events |> create_msg() |> send_email()
+  #   end
+  # end
+
+  @doc """
+  Filters out events that are non recurring if the date is today.
+  Notifies if event list is empty after filtering
+  """
+  def filter_events({_date, []}) do
+    :no_events
+  end
+  def filter_events({date, event_list}) do
+    case Enum.filter(event_list,
+	  fn
+	    {_id, true, _desc} -> true
+	    {_id, false, _desc} -> if Date.utc_today() == date, do: true, else: false
+	  end
+	) do
+      [] -> :no_events
+      filtered_list -> filtered_list
+    end
+  end
+
+  @doc """
+  Formulates the message to be sent as email to user.
+  """
+  def create_message(event_list) do
+    event_to_msg = fn {event, _recur, desc} -> "#{event} - #{desc}" end
+    """
+    Events for today (#{Date.utc_today()}):
+
+    #{event_list |> Enum.map(event_to_msg) |> Enum.join("\n")}
+    """
+  end
+
+  @doc """
+  sends the message as email to the address (defined in config)
+  """
+  def send_email(message) do
+    nil
+  end
+  
+end
+
+	    
--- test/reminder_events_test.exs
+++ test/reminder_events_test.exs
@@ -3,7 +3,7 @@ defmodule ReminderEventsTest do
   doctest Reminder
   alias Reminder.Events
 
-  describe "event list filtering checks" do
+  describe "filter_events tests" do
 
     test "event list empty" do
       assert Events.filter_events({~D[2010-09-10], []}) == :no_events
@@ -16,13 +16,35 @@ defmodule ReminderEventsTest do
              ) == [{"Event1", false, "Is an event"}, {"Event2", true, "Is also an event"}]
     end
 
+    test ":no_event returned if event list is empty after filtering" do
+      assert Events.filter_events(
+               {~D[2010-12-12],
+                [{"Event1", false, "Is an event"}, {"Event2", false, "Is also an event"}]}
+             ) == :no_events
+    end
+
     test "non recurring event in list is removed if date is earlier than today" do
       assert Events.filter_events(
                {~D[2010-01-01],
                 [{"Event1", false, "Is an event"}, {"Event2", true, "Is also an event"}]}
              ) == [{"Event2", true, "Is also an event"}]
     end
+
   end
+
+  test "create_message() produces correct message" do
+    event_list = [{"Event1", false, "Is an event"}, {"Event2", true, "Is also an event"}]
+    event_message = """
+    Events for today (#{Date.utc_today()}):
+    
+    Event1 - Is an event
+    Event2 - Is also an event
+    """
+    assert Events.create_message(event_list) == event_message
+  end
+  
+  # describe "send_reminders pipeline tests" do
+  #   test 
 
   test "greets the world" do
     assert Reminder.hello() == :world
