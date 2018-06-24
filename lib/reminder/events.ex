11e8a91ec87646bd0a1828f72e6fb8cc5845abb7
Subil <subil1407@gmail.com>
2018-06-24 09:33:18 -0400

    filter_events tests written

--- lib/reminder/events.ex
+++ lib/reminder/events.ex
@@ -1,7 +1,6 @@
 # REMINDERS:
 # set priorities for the events
 defmodule Reminder.Events do
-
   # def send_events_reminder(event_data) do
   #   case filter_events(event_data) do
   #     :no_events -> quit
@@ -15,16 +14,32 @@ defmodule Reminder.Events do
   of the form {event name, recurring (true/false), priority (1,2), description}.
   """
   def filter_non_recurring_events({date, event_list}) do
-    case Enum.filter(event_list,
-	  fn
-	    {_id, true, _priority, _desc} -> true
-	    {_id, false, _priority, _desc} -> if Date.utc_today() == date, do: true, else: false
-	  end
-	) do
-      [] -> {date, []}
-      filtered_list -> {date, filtered_list}
-    end
+    {date,
+     Enum.filter(event_list, fn
+       {_id, true, _priority, _desc} -> true
+       {_id, false, _priority, _desc} -> if date >= Date.utc_today(), do: true, else: false
+     end)}
   end
+
+  @doc """
+  Filter out events that are low priority (i.e. priority 1). Used only with the next_week events.
+  """
+  def filter_low_priority_events({date, event_list}) do
+    {date,
+     Enum.filter(event_list, fn
+       {_id, _recur, 1, _desc} -> false
+       _ -> true
+     end)}
+  end
+
+  @doc """
+  Identifies if the event list is empty. Returns :no_event if empty, otherwise returns
+  {date, event_list}.
+  """
+  def filter_empty({date, event_list}) when event_list == [] do
+    :no_events
+  end
+  def filter_empty({date, event_list}), do: {date, event_list}
 
   @doc """
   Formulates the message to be sent as email to user.
@@ -34,7 +49,7 @@ defmodule Reminder.Events do
       subject: "Reminders for today",
       from: Application.get_env(:reminder, :from_email),
       to: [Application.get_env(:reminder, :to_email)],
-      text: "hello",
+      text: "hello"
     }
   end
 
@@ -46,4 +61,3 @@ defmodule Reminder.Events do
   end
 end
 
-	    
--- test/reminder_events_test.exs
+++ test/reminder_events_test.exs
@@ -3,6 +3,158 @@ defmodule ReminderEventsTest do
   doctest Reminder
   alias Reminder.Events
 
+  describe "filter_events tests:" do
+    setup do
+      today = Date.utc_today()
+      tomorrow = Date.add(Date.utc_today(), 1)
+      next_week = Date.add(Date.utc_today(), 7)
+      today_erl = Date.to_erl(today)
+      tomorrow_erl = Date.to_erl(tomorrow)
+      next_week_erl = Date.to_erl(next_week)
+
+      [
+        today_data: [
+          # today
+          {today_erl,
+           [
+             {"Event1", false, 1, "Is an event"},
+             {"Event2", true, 1, "Is also an event"},
+             {"Event3", false, 2, "Is another event"},
+             {"Event4", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as today but past year
+          {{2012, today.month, today.day},
+           [
+             {"Event5", false, 1, "Is an event"},
+             {"Event6", true, 1, "Is also an event"},
+             {"Event7", false, 2, "Is another event"},
+             {"Event8", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as today but future year
+          {{2100, today.month, today.day},
+           [
+             {"Event9", false, 1, "Is an event"},
+             {"Event10", true, 1, "Is also an event"},
+             {"Event11", false, 2, "Is another event"},
+             {"Event12", true, 2, "Is yet another event"}
+           ]}
+        ],
+        tomorrow_data: [
+          # tomorrow
+          {tomorrow_erl,
+           [
+             {"Event1", false, 1, "Is an event"},
+             {"Event2", true, 1, "Is also an event"},
+             {"Event3", false, 2, "Is another event"},
+             {"Event4", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as tomorrow but past year
+          {{2012, tomorrow.month, tomorrow.day},
+           [
+             {"Event5", false, 1, "Is an event"},
+             {"Event6", true, 1, "Is also an event"},
+             {"Event7", false, 2, "Is another event"},
+             {"Event8", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as tomorrow but future year
+          {{2100, tomorrow.month, tomorrow.day},
+           [
+             {"Event9", false, 1, "Is an event"},
+             {"Event10", true, 1, "Is also an event"},
+             {"Event11", false, 2, "Is another event"},
+             {"Event12", true, 2, "Is yet another event"}
+           ]}
+        ],
+        next_week_data: [
+          # next_week
+          {next_week_erl,
+           [
+             {"Event1", false, 1, "Is an event"},
+             {"Event2", true, 1, "Is also an event"},
+             {"Event3", false, 2, "Is another event"},
+             {"Event4", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as next_week but past year
+          {{2012, next_week.month, next_week.day},
+           [
+             {"Event5", false, 1, "Is an event"},
+             {"Event6", true, 1, "Is also an event"},
+             {"Event7", false, 2, "Is another event"},
+             {"Event8", true, 2, "Is yet another event"}
+           ]},
+          # same day and month as next_week but future year
+          {{2100, next_week.month, next_week.day},
+           [
+             {"Event9", false, 1, "Is an event"},
+             {"Event10", true, 1, "Is also an event"},
+             {"Event11", false, 2, "Is another event"},
+             {"Event12", true, 2, "Is yet another event"}
+           ]}
+        ],
+        empty_event_data: [
+          {today_erl, []},
+          {{2012, next_week.month, next_week.day},
+           [
+             {"Event5", false, 1, "Is an event"},
+             {"Event6", true, 1, "Is also an event"},
+             {"Event7", false, 2, "Is another event"}
+           ]}
+        ]
+      ]
+    end
+
+    test "today's events are filtered out correctly and final event list is produced", fixture do
+      assert MapSet.equal?(
+               MapSet.new(Events.filter_events(fixture.today_data)),
+               MapSet.new([
+                 {"Event1", false, 1, "Is an event"},
+                 {"Event2", true, 1, "Is also an event"},
+                 {"Event3", false, 2, "Is another event"},
+                 {"Event4", true, 2, "Is yet another event"},
+                 {"Event6", true, 1, "Is also an event"},
+                 {"Event8", true, 2, "Is yet another event"}
+                 # future events must not appear
+               ])
+             )
+    end
+
+    test "tomorrow's events are filtered out correctly and final event list is produced",
+         fixture do
+      assert MapSet.equal?(
+               MapSet.new(Events.filter_events(fixture.tomorrow_data)),
+               MapSet.new([
+                 {"Event1", false, 1, "Is an event"},
+                 {"Event2", true, 1, "Is also an event"},
+                 {"Event3", false, 2, "Is another event"},
+                 {"Event4", true, 2, "Is yet another event"},
+                 {"Event6", true, 1, "Is also an event"},
+                 {"Event8", true, 2, "Is yet another event"}
+                 # future events must not appear
+               ])
+             )
+    end
+
+    test "next week's events are filtered out correctly and final event list is produced",
+         fixture do
+      assert MapSet.equal?(
+               MapSet.new(Events.filter_events(fixture.next_week_data)),
+               MapSet.new([
+                 {"Event3", false, 2, "Is another event"},
+                 {"Event4", true, 2, "Is yet another event"},
+                 {"Event8", true, 2, "Is yet another event"}
+                 # future events must not appear
+               ])
+             )
+    end
+
+    test "filter_events returns :no_events if final event list is empty", fixture do
+      assert Mapset.equal?(
+               MapSet.new(Events.filter_events(fixture.empty_event_data)),
+               MapSet.new([])
+             )
+    end
+  end
+
   describe "filter_low_priority_events tests:" do
     setup do
       [
@@ -45,7 +197,9 @@ defmodule ReminderEventsTest do
         test_events: [
           {"Event1", false, 1, "Is an event"},
           {"Event2", true, 2, "Is also an event"}
-        ]
+        ],
+        later_date: Date.add(Date.utc_today(), 4),
+        earlier_date: Date.add(Date.utc_today(), -4)
       ]
     end
 
@@ -56,6 +210,12 @@ defmodule ReminderEventsTest do
     test "non recurring event in list is preserved if date is today or later", fixture do
       assert Events.filter_non_recurring_events({Date.utc_today(), fixture.test_events}) ==
                {Date.utc_today(), fixture.test_events}
+
+      assert Events.filter_non_recurring_events({fixture.later_date, fixture.test_events}) ==
+               {fixture.later_date, fixture.test_events}
+
+      refute Events.filter_non_recurring_events({fixture.earlier_date, fixture.test_events}) ==
+               {fixture.earlier_date, fixture.test_events}
     end
 
     test "empty list returned if event list is empty after filtering", _fixture do
@@ -108,7 +268,7 @@ defmodule ReminderEventsTest do
   end
 
   test "email is configured and sent" do
-    # not testing this separately for now. Works good enough.
+    # not testing this separately for now. email sending works good enough.
     nil
   end
 
