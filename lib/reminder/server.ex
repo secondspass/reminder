9883d0ea77dd96532666a739e081d33d7531c1d4
Subil <subil1407@gmail.com>
2018-07-04 12:34:34 -0400

    Some server and test code done

--- lib/reminder/server.ex
+++ lib/reminder/server.ex
@@ -2,17 +2,22 @@ defmodule Reminder.Server do
   use GenServer
 
   def init(_) do
-    {:ok, file_db} = :dets.open_file(:reminders, [{:file, Application.get_env(:reminder, :db)}])
-    db = :ets.from_dets(:ets.new(:reminders, []), file_db)
+    {:ok, file_db} = :dets.open_file(:reminders_dets, [{:file, Application.get_env(:reminder, :db)}])
+    db = :ets.new(:reminders, [])
+    :ets.from_dets(db, file_db)
     {:ok, db}
   end
 
   def start_link() do
     GenServer.start_link(__MODULE__, nil, name: :rem_server)
   end
-  
-  def insert_event do
-    nil
+
+  def insert_event(date_erl, eventname, recurring, priority, desc) do
+    GenServer.call(:rem_server, {:insert, date_erl, {eventname, recurring, priority, desc}})
+  end
+
+  def sync_to_dets do
+    GenServer.cast(:rem_server, :sync)
   end
 
   @doc """
@@ -22,7 +27,7 @@ defmodule Reminder.Server do
     date = Date.utc_today()
     GenServer.call(:rem_server, {:get, date.day, date.month})
   end
-  
+
   def get_tomorrow do
     date = Date.add(Date.utc_today(), 1)
     GenServer.call(:rem_server, {:get, date.day, date.month})
@@ -33,9 +38,31 @@ defmodule Reminder.Server do
     GenServer.call(:rem_server, {:get, date.day, date.month})
   end
 
+  def handle_call({:insert, date, eventdetails}, _, db) do
+    status =
+      case :ets.lookup(db, date) do
+        [] ->
+          :ets.insert(db, {date, [eventdetails]}) && :inserted
+
+        [{^date, eventlist}] ->
+	  if eventdetails not in eventlist do
+            :ets.insert(db, {date, [eventdetails | eventlist]}) && :inserted
+	  else
+	    :event_already_present
+	  end
+      end
+
+    {:reply, status, db}
+  end
+
   def handle_call({:get, day, month}, _, db) do
     event_data = :dets.match_object(db, {{:"$1", day, month}, :"$2"})
     {:reply, event_data, db}
   end
 
+  def handle_cast(:sync, db) do
+    :ets.to_dets(db, :reminders_dets)
+    {:noreply, db}
+  end
+  
 end
--- test/reminder_server_test.exs
+++ test/reminder_server_test.exs
@@ -3,9 +3,52 @@ defmodule ReminderServerTest do
   doctest Reminder
   alias Reminder.Server
 
+  describe "Get events from test db:" do
+    setup do
+      fixture = [
+        rem_serv: elem(Server.start_link(), 1)
+      ]
+      fixture
+    end
+
+    test "Insert events and dets sync", fixture do
+      # what i want to do here is start the genserver and get the event through the interface. Before that
+      # get the event directly from the db. Then compare if the two results are the same
+      # maybe use insert_event to insert an event for today, then use get_today to get that event and check if it is the same event inserted
+      # we are only testing data storage and retrieval. No need to worry about today, tomorrow and all that, i think.
+      event1 = %{
+               date: Date.to_erl(Date.utc_today()),
+               name: "today event #{Date.utc_today()} #{:rand.normal()}",
+               recur: true,
+               priority: 1,
+               desc: "description #{Date.utc_today()} #{:rand.normal()}"
+      }
+      # same day, different event
+      event2 = %{
+               date: Date.to_erl(Date.utc_today()),
+               name: "today event #{Date.utc_today()} #{:rand.normal()}",
+               recur: true,
+               priority: 1,
+               desc: "description #{Date.utc_today()} #{:rand.normal()}"
+	    }
+      # insert some event for today (we have not persisted the db yet)
+      assert Server.insert_event(event1.date, event1.name, event1.recur, event1.priority, event1.desc) == :inserted
+      assert Server.insert_event(event2.date, event2.name, event2.recur, event2.priority, event2.desc) == :inserted
+      # check if the event above has been inserted
+      assert Server.insert_event(event1.date, event1.name, event1.recur, event1.priority, event1.desc) == :event_already_present
+      # sync ets to dets on file
+      Server.sync_to_dets()
+      # since sync_to_dets is a cast (async operation), we need to force sync by calling :sys.get_state in order to properly open and check the dets to
+      # see if the values from the ets were inserted
+      test_ets = :sys.get_state(fixture.rem_serv)
+      # need to use the already opened dets from the genserver to check the values, hence :reminders_dets
+      assert :dets.lookup(:reminders_dets, event1.date) == :ets.lookup(test_ets, event2.date)
+
+    end
+
+    test "dets sync" do
+      nil
+    end
+    
+  end
 end
-
-
-  
-    
-    
